30d40655afe13ee9f8e03cfa57ce2d5e24f7874b
Da00 <morgan180@protonmail.com>
2018-07-20 21:38:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ mi_socket=socket.socket()
 mi_socket.bind(('localhost',8080))
 mi_socket.listen(5)
 
+for i in range(5):
+    print("%d \n",i)
 while True:
     conexion,addr=mi_socket.accept()
     print('nueva conexión')
