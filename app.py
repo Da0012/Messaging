391d8e7d55ab86403a4c736d458b3c5aebd3f37a
Da00 <morgan180@protonmail.com>
2018-07-20 20:39:49 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import socket
+mi_socket=socket.socket()
+mi_socket.bind(('localhost',8000))
+mi_socket.listen(5)
+
+while True:
+    conexion,addr=mi_socket.accept()
+    print('nueva conexión')
+    print(addr)
+    
+    conexion.send("soy el servidor".encode())
+    
+    conexion.close()
